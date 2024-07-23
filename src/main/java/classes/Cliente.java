77caf6b2741f964ca50e3d09899f5900732968fb
Aline Lemos <alinelemos@id.uff.br>
2024-07-23 20:17:56 -0300

    crud

---
+++ src/main/java/classes/Cliente.java
@@ -0,0 +1,15 @@
+package classes;
+
+import lombok.Data;
+
+@Data
+public class Cliente {
+
+    private String nome;
+    private String cpf;
+    private Endereco endereco;
+    private String telefone;
+    private String email;
+    private String dataNascimento;
+
+}
